d30b3bbe124d4c77de9eeb04e692ce52dbb1907a
Pio Rasch-Halvorsen <pio.rasch-halvorsen@fremtind.no>
2025-06-15 22:55:14 +0200

    feat: use css variables for colors and add support for theming

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,7 +9,7 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
+    <html lang="en" data-theme="amber">
       <body>{children}</body>
     </html>
   )
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 "use client"
 
 import Phosphor from "@/components/Phosphor";
+import "@/styles/styles.css";
 
 export default function RootPage() {
     return <Phosphor />;
